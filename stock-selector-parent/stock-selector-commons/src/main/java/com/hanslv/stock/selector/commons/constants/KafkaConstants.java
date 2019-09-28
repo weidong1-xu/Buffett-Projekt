8c37a6d84bf6a398b25f3c117dae4068488a9b81
Cheng Lv <627455824@qq.com>
2019-09-28 23:50:13 +0800

    hanslv

---
+++ stock-selector-parent/stock-selector-commons/src/main/java/com/hanslv/stock/selector/commons/constants/KafkaConstants.java
@@ -0,0 +1,13 @@
+package com.hanslv.stock.selector.commons.constants;
+/**
+ * Kafka相关常量
+ * 
+ * ------------------------------------
+ * 1、Consumer阻塞队列大小										CONSUMER_BLOCKINGQUEUE_SIZE
+ * ------------------------------------
+ * @author hanslv
+ *
+ */
+public class KafkaConstants {
+	public static final int CONSUMER_BLOCKINGQUEUE_SIZE = 5000;//Consumer阻塞队列大小
+}
--- stock-selector-parent/stock-selector-commons/src/main/java/com/hanslv/stock/selector/commons/util/KafkaUtil.java
+++ stock-selector-parent/stock-selector-commons/src/main/java/com/hanslv/stock/selector/commons/util/KafkaUtil.java
@@ -3,18 +3,34 @@ package com.hanslv.stock.selector.commons.util;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.time.Duration;
+import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.jboss.logging.Logger;
+
+import com.hanslv.stock.selector.commons.constants.KafkaConstants;
 
 /**
  * Kafka工具类
- * 需要在引入commons模块的其他模块下自行配置kafka.properties
- * ！！！注意：一个KafkaUtil实例只可充当KafkaProducer或KafkaConsumer之一，不可得兼，除非调用关闭连接方法，否则会出现异常！！！
+ * 需要在引入commons模块的其他模块下自行配置kafka.properties，
+ * 采取非阻塞的方式，在Producer发送消息、Consumer接收消息时都会创建新线程来完成工作，与主线程分开，
+ * 每个KafkaUtil实例只支持实例化一个Consumer，多个会出现异常
+ * 在使用后需要关闭资源
  * --------------------------------------------
- * 
+ * 1、关闭当前currentProducerInstance										public void closeProducerConnection()
+ * 2、关闭当前currentConsumerInstance										public void closeConcumerConnection()
+ * 3、向指定的topic发送多条消息												public void sendMessage(String topic , K key , List<V> messageList , Callback callbackLogic)
+ * 4、从Topic中获取消息														public void pollMessage(List<String> topicList , long timeout)
+ * 5、从Consumer消息队列中获取一个消息										public V takeValueFromConsumerBlockingQueue()
  * --------------------------------------------
  * @author hanslv
  *
@@ -32,6 +48,12 @@ public class KafkaUtil<K , V> {
 	 */
 	private KafkaProducer<K , V> currentProducerInstance;
 	private KafkaConsumer<K , V> currentConsumerInstance;
+	
+	
+	/*
+	 * 记录Consumer消费消息的消息队列
+	 */
+	private BlockingQueue<V> consumerBlockingQueue;
 
 	
 	/**
@@ -52,100 +74,195 @@ public class KafkaUtil<K , V> {
 	}
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	/**
+	 * 1、关闭当前currentProducerInstance
+	 */
+	public void closeProducerConnection() {
+		if(currentProducerInstance != null) {
+			currentProducerInstance.close();
+			currentProducerInstance = null;
+			logger.info(Thread.currentThread() + " 关闭了一个KafkaProducer");
+		}
+	}
 	
 	/**
-	 * 创建一个KafkaProducer实例，
-	 * 如果已经使用当前KafkaUtil创建过一个并且当前是打开状态的KafkaProducer实例，则会返回该KafkaProducer实例，
-	 * 否则会创建一个新的实例
-	 * @return
+	 * 2、关闭当前currentConsumerInstance
 	 */
-	private KafkaProducer<K , V> createKafkaProducer(){
+	public void closeConcumerConnection() {
 		if(currentConsumerInstance != null) {
-			try {
-				throw new Exception("当前KafkaUtil已经充当Consumer，不可以同时使用Producer相关方法，若需要请先关闭当前的Consumer连接！");
-			} catch (Exception e) {
-				e.printStackTrace();
+			if(consumerBlockingQueue.size() > 0) {
+				try {
+					throw new Exception("当前Consumer消息队列中仍然存在未消费消息！");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				return;
 			}
-		}
-		if(currentProducerInstance != null) 
-			return currentProducerInstance;
-		else {
-			logger.info(Thread.currentThread() + " 创建了一个KafkaProducer");
-			currentProducerInstance = new KafkaProducer<>(currentProp);
-			return currentProducerInstance;
+			currentConsumerInstance.close();
+			currentConsumerInstance = null;
+			logger.info(Thread.currentThread() + " 关闭了一个KafkaConsumer");
 		}
 	}
 	
 	
 	/**
-	 * 创建一个KafkaConsumer实例，
-	 * 如果已经使用当前KafkaUtil创建过一个并且当前是打开状态的KafkaConsumer实例，则会返回该KafkaConsumer实例，
-	 * 否则会创建一个新的实例
+	 * 3、向指定的topic发送多条消息
+	 * 创建一个新线程，采用异步发送的方式，在新创建的线程中处理可重试异常
+	 * @param topic 
+	 * @param key partition策略Key
+	 * @param messageList 包含全部要发送消息的集合
+	 * @param callbackLogic 重试逻辑
+	 */
+	public void sendMessage(String topic , K key , List<V> messageList , Callback callbackLogic) {
+		/*
+		 * 实例化currentProducerInstance
+		 */
+		createKafkaProducer();
+		new Thread(() -> {
+			/*
+			 * 遍历要发送的消息List
+			 */
+			for(V message : messageList) {
+				logger.info(Thread.currentThread() + " 向Topic：" + topic + "发送了一条消息：" + String.valueOf(message));
+				currentProducerInstance.send(new ProducerRecord<>(topic , key , message) , callbackLogic);
+			}
+		}).start();
+	}
+	
+	
+	/**
+	 * 4、从Topic中获取消息
+	 * 将会创建一个新线程并创建Consumer订阅指定的Topic，并将接收到的Topic写入到消息队列中
+	 * @param topicList
+	 * @param timeout
 	 * @return
 	 */
-	private KafkaConsumer<K , V> createKafkaConsumer(){
-		if(currentProducerInstance != null) {
+	public void pollMessage(List<String> topicList , long timeout) {
+		/*
+		 * 实例化currentConsumerInstance
+		 */
+		createKafkaConsumer(topicList);
+		
+		/*
+		 * 启动新线程订阅
+		 */
+		new Thread(() -> {
+			/*
+			 * 订阅消息并写入到消息队列
+			 */
+			while(true) {
+				ConsumerRecords<K , V> records = currentConsumerInstance.poll(Duration.ofMillis(timeout));
+				for(ConsumerRecord<K , V> record : records) {
+					try {
+						/*
+						 * 存入消息队列
+						 */
+						consumerBlockingQueue.put(record.value());
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}).start();
+	}
+	
+	
+	/**
+	 * 5、从Consumer消息队列中获取一个消息
+	 * @return
+	 */
+	public V takeValueFromConsumerBlockingQueue() {
+		try {
+			return consumerBlockingQueue.take();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * 实例化currentProducerInstance
+	 */
+	private void createKafkaProducer(){
+		if(currentProducerInstance == null) {
+			logger.info(Thread.currentThread() + " 创建了一个KafkaProducer");
+			currentProducerInstance = new KafkaProducer<>(currentProp);
+		}
+	}
+	
+	
+	/**
+	 * 实例化currentConsumerInstance
+	 * @param topicList 订阅的TopicList
+	 */
+	private void createKafkaConsumer(List<String> topicList){
+		if(currentConsumerInstance == null) {
+			/*
+			 * 实例化Consumer消息队列
+			 */
+			consumerBlockingQueue = new ArrayBlockingQueue<>(KafkaConstants.CONSUMER_BLOCKINGQUEUE_SIZE);
+			
+			logger.info(Thread.currentThread() + " 创建了一个KafkaConsumer");
+			currentConsumerInstance = new KafkaConsumer<>(currentProp);
+			
+			/*
+			 * 设置当前Consumer订阅某些Topic
+			 */
+			currentConsumerInstance.subscribe(topicList);
+		}else {
 			try {
-				throw new Exception("当前KafkaUtil已经充当Producer，不可以同时使用Consumer相关方法，若需要请先关闭当前的Producer连接！");
+				throw new Exception("只支持实例化一个Consumer，多个会出现异常！");
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		}
-		if(currentConsumerInstance != null) 
-			return currentConsumerInstance;
-		else {
-			logger.info(Thread.currentThread() + " 创建了一个KafkaProducer");
-			currentConsumerInstance = new KafkaConsumer<>(currentProp);
-			return currentConsumerInstance;
 		}
 	}
 	
