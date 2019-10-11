5abe7fa82a2c1a9bc24381cd500e6e5b475190b4
harry4922 <627455824@qq.com>
2019-10-11 10:32:20 +0800

    hanslv

--- stock-selector-parent/stock-selector-algorithm/src/main/java/com/hanslv/stock/selector/algorithm/AlgorithmLogic.java
+++ stock-selector-parent/stock-selector-algorithm/src/main/java/com/hanslv/stock/selector/algorithm/AlgorithmLogic.java
@@ -8,6 +8,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.jboss.logging.Logger;
 import org.springframework.stereotype.Component;
 
 import com.hanslv.stock.selector.algorithm.constants.AlgorithmOtherConstants;
@@ -19,7 +20,7 @@ import com.hanslv.stock.selector.commons.util.MyBatisUtil;
 
 /**
  * 算法模板
- * 包含一个公用线程池、需要执行的算法队列、执行完毕算法计数器、当前日期String
+ * 包含一个公用线程池、需要执行的算法队列、执行完毕算法计数器、当前日期
  * 
  * 执行前准备：
  * 实例化公用线程池
@@ -27,7 +28,7 @@ import com.hanslv.stock.selector.commons.util.MyBatisUtil;
  * 实例化执行完毕算法计数器
  * 查询数据库中全部的算法ID、算法名称、算法类全名、算法时间区间并放入需要执行的算法队列中并将计数器+1
  * 
- * 调用runAlgorithm()方法执行全部算法：
+ * runAlgorithm()方法逻辑：
  * 提交N个任务到线程池：
  *  获取当前计数器的值
  * 	while(算法计数器!=0)循环：
@@ -44,13 +45,15 @@ import com.hanslv.stock.selector.commons.util.MyBatisUtil;
  * 						立即关闭线程池（shutDownNow）
  * 
  * 
- * 新创建的算法需要在数据库中初始化包括（算法名称、算法类全名、算法时间区间）
+ * 新创建的算法需要在数据库中初始化，包括（算法名称、算法类全名、算法时间区间）
  * 子类需要重写当前算法的算法逻辑algorithmLogic()
  * 
  * @author hanslv
  */
 @Component
 public class AlgorithmLogic {
+	static Logger logger = Logger.getLogger(AlgorithmLogic.class);
+	
 	/*
 	 * 公用线程池，调用runAlgorithm()方法向该线程池提交一个算法逻辑并运算
 	 */
@@ -67,38 +70,40 @@ public class AlgorithmLogic {
 	private static BlockingQueue<TabAlgorithmInfo> incompleteBlockingQueue;
 	
 	/*
-	 * 当前日期String
+	 * 当前日期
 	 */
 	private static LocalDate currentDate;
-	
-	/*
-	 * 初始化
-	 */
-	static {
-		publicThreadPool = Executors.newFixedThreadPool(AlgorithmOtherConstants.ALGORITHM_THREAD_POOL_SIZE);
-		counter = new AtomicInteger();
-		incompleteBlockingQueue = new ArrayBlockingQueue<>(CommonsOtherConstants.BASIC_BLOCKING_QUEUE_SIZE);
-		initAlgorithms();
-	}
-	
 	
 	/**
 	 * 运行全部算法
 	 */
 	public static void runAlgorithm() {
 		/*
+		 * 初始化
+		 */
+		initAlgorithms();
+		
+		/*
 		 * 提交N个任务
 		 */
 		for(int i = 0 ; i < AlgorithmOtherConstants.ALGORITHM_THREAD_POOL_SIZE ; i++) {
 			publicThreadPool.execute(() -> {
 				TabAlgorithmResultRepository algorithmResultMapper = MyBatisUtil.getInstance().getConnection().getMapper(TabAlgorithmResultRepository.class);
-				int currentCounter = 0;
 				
 				/*
 				 * 获取当前计数器，while(算法计数器!=0)循环
+				 * 
+				 * 保持线程池最后被关闭的思路：
+				 * counter>0，则说明当前队列中仍然存在算法信息，或仍然有算法正在被其他线程计算
+				 * 
+				 * 当有线程正在计算算法时counter不可能为0。
+				 * 因为有线程正在计算说明当前存在未完成计算的算法，并且该算法对象是唯一的，只有在执行完当前算法时才可能对counter进行操作
+				 * 
+				 * 若当前存在未完成算法（counter不为0）但是该算法对象被其他线程持有，当前算法可能进入while循环，并且在从阻塞队列中取出算法信息时阻塞，
+				 * 但最终会因为其他线程执行完毕后插入新的算法信息而继续，或因其他线程判断当前算法已经执行完毕并且将counter-1得0而关闭线程池结束
 				 */
 				try {
-					while((currentCounter = getCounter()) >= 0) {
+					while(getCounter() > 0) {
 						TabAlgorithmInfo currentAlgorithmInfo = takeFromIncomplateBlockingQueue();
 						
 						/*
@@ -111,12 +116,37 @@ public class AlgorithmLogic {
 						 */
 						if(!checkLastRunDate(currentAlgorithmInfo.getAlgorithmDayCount() , lastRunDate)) {
 							/**
-							 * 执行当前算法的algorithmLogic()方法
+							 * 利用反射执行当前算法的algorithmLogic()方法
 							 */
+							try {
+								AlgorithmLogic currentAlgorithm = (AlgorithmLogic) Class.forName(currentAlgorithmInfo.getAlgorithmClassName()).newInstance();
+								currentAlgorithm.algorithmLogic(lastRunDate);
+								
+								/*
+								 * 将当前算法放回阻塞队列
+								 */
+								putIncomplateAlgorithmInfo(currentAlgorithmInfo);
+								
+							} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+								e.printStackTrace();
+							}
+						}else {
+							/*
+							 * 执行完毕计数器-1
+							 */
+							decrCounter();
+							
+							/*
+							 * 判断当前算法计数器是否为0，是则关闭线程池
+							 */
+							if(getCounter() == 0) shutdownThreadPool();
+							
 						}
-						
 					}
 				}finally {
+					/*
+					 * 当前线程结束后关闭数据库连接
+					 */
 					MyBatisUtil.getInstance().closeConnection();
 				}
 			});
@@ -139,15 +169,16 @@ public class AlgorithmLogic {
 	
 	/**
 	 * 需要子类实现的算法逻辑
+	 * 数据库连接交由外层runAlgorithm()方法管理，方法中不可关闭
 	 */
-	void algorithmLogic(TabAlgorithmInfo currentAlgorithmInfo) {}
+	void algorithmLogic(String lastRunDate) {}
 	
 	
 	/**
 	 * 关闭公用线程池
 	 */
 	static void shutdownThreadPool() {
-		publicThreadPool.shutdown();
+		if(!publicThreadPool.isShutdown()) publicThreadPool.shutdown();
 	}
 	
 	
@@ -160,9 +191,21 @@ public class AlgorithmLogic {
 		try {
 			incomplateAlgorithmInfo = incompleteBlockingQueue.take();
 		} catch (InterruptedException e) {
-			e.printStackTrace();
+			logger.info("线程池关闭，停止等待从incompleteBlockingQueue中获取算法信息........");
 		}
 		return incomplateAlgorithmInfo;
+	}
+	
+	/**
+	 * 向队列中放入未完成算法信息
+	 * @param incomplateAlgorithmInfo
+	 */
+	static void putIncomplateAlgorithmInfo(TabAlgorithmInfo incomplateAlgorithmInfo) {
+		try {
+			incompleteBlockingQueue.put(incomplateAlgorithmInfo);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 	
 
@@ -181,6 +224,9 @@ public class AlgorithmLogic {
 	static int getCounter() {
 		return counter.get();
 	}
+
+	
+	
 	
 	
 	
@@ -223,9 +269,13 @@ public class AlgorithmLogic {
 	
 	
 	/*
-	 * 初始化未完成算法队列
+	 * 初始化
 	 */
 	private static void initAlgorithms() {
+		publicThreadPool = Executors.newFixedThreadPool(AlgorithmOtherConstants.ALGORITHM_THREAD_POOL_SIZE);
+		counter = new AtomicInteger();
+		incompleteBlockingQueue = new ArrayBlockingQueue<>(CommonsOtherConstants.BASIC_BLOCKING_QUEUE_SIZE);
+		
 		/*
 		 * 获取全部算法信息LIst
 		 */
@@ -245,22 +295,6 @@ public class AlgorithmLogic {
 		 */
 		currentDate = LocalDate.now();
 	}
-	
-	
-	
-	
-	/**
-	 * 向队列中放入未完成算法信息
-	 * @param incomplateAlgorithmInfo
-	 */
-	private static void putIncomplateAlgorithmInfo(TabAlgorithmInfo incomplateAlgorithmInfo) {
-		try {
-			incompleteBlockingQueue.put(incomplateAlgorithmInfo);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-	
 	
 	
 	/**
---
+++ stock-selector-parent/stock-selector-algorithm/src/test/java/com/hanslv/stock/selector/algorithm/test/TestReflex2.java
@@ -0,0 +1,8 @@
+package com.hanslv.stock.selector.algorithm.test;
+
+public class TestReflex2 extends TestReflex{
+	@Override
+	public void test() {
+		System.out.println("This is test2");
+	}
+}
